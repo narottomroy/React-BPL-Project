2d7a69b4b2a06cec2960451186ebdda989301f88
Narottom Roy <narottombd05@gmail.com>
2026-09-09 21:33:51 +0600

    banner design is complite for home page

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -1,11 +1,45 @@
-
+import bgShadow from "../assets/bg-shadow.png";
+import bannerImg from "../assets/banner-main.png"
 
 const Banner = () => {
-    return (
-        <div className='min-h-24 bg-purple-500 my-7 flex justify-center items-center'>
-            <h2 className='font-bold text-6xl'>BANNER</h2>
-        </div>
-    );
+  return (
+    <div
+      className="
+        min-h-[500px]
+        my-7
+        flex
+        flex-col
+        justify-center
+        items-center
+        bg-black
+        bg-no-repeat
+        bg-cover
+        bg-center
+      "
+      style={{
+        backgroundImage: `url(${bgShadow})`,
+      }}
+    >
+        <img className="
+        justify-center
+        items-center
+        bg-no-repeat
+        bg-cover
+        bg-center" 
+        src={bannerImg} alt="" />
+      <h2 className="text-4xl md:text-6xl font-bold text-white text-center">
+        Assemble Your Ultimate Dream 11 Cricket Team
+      </h2>
+
+      <p className="mt-4 text-xl text-gray-300">
+        Beyond Boundaries Beyond Limits
+      </p>
+
+      <button className="mt-6 rounded-xl bg-lime-400 px-7 py-3 font-bold text-black">
+        Claim Free Credit
+      </button>
+    </div>
+  );
 };
 
 export default Banner;
